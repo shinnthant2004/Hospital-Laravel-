792a29b40e99c95c5dd5506252165e4323902665
shinthantequi@gmail.com <shinthantequi@gmail.com>
2022-04-29 10:58:23 +0630

    email verify and forget password complete

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[HomeController::class,'index']);
-Route::get('/home',[HomeController::class,'redirect']);
+Route::get('/home',[HomeController::class,'redirect'])->middleware('auth','verified');
 
 Route::get('/add_doctor',[AdminController::class,'add_doctor']);
 Route::post('/add_doctor',[AdminController::class,'upload_doctor']);
